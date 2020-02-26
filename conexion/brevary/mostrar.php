87ca97b10f77a3df0b6416c25dfc1cb6e0480015
Ricardo <ricardoandres.fonseca@gmail.com>
2020-02-25 19:00:39 -0500

    Se agrega breviario.php y carpeta conexiones categoria breviary

---
+++ conexion/brevary/mostrar.php
@@ -0,0 +1,16 @@
+<?php
+//session_start();
+require_once("conexionDB.php");
+
+$language = $_SESSION["language"]; 
+$fechaActual = date('Y-m-d');
+//$type = $_POST['brevaryDB'];
+
+$objConexion = new ConexionDB();
+
+//Sentencia para realizar consulta
+$sql = "SELECT * FROM Breviario WHERE DateLiturgia = '$fechaActual' AND Type = 'Oficios de Lectura' AND language = '$language'";
+$resultado = $objConexion->consulta($sql);
+echo $resultado['TextB'];
+	
+?>
