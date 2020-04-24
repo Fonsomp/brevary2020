ad0b659e63cff8c4ebe23fe713717a22dc69f8e0
Ricardo <ricardoandres.fonseca@gmail.com>
2020-04-24 12:12:45 -0500

    prueba GPS

---
+++ scripts/pruebaGPS.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <script type="text/javascript" src="gps2.js"></script>
+</head>
+<body>
+    <h1>prueba gps</h1>
+    <p id="geolocation-test"></p>
+    <input type="button" onclick="mostrargps()">
+</body>
+
+</html>
