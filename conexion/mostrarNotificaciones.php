7ec8b04f8056884e25ed3cde408f3d0b067df081
Fonsomp <59423204+Fonsomp@users.noreply.github.com>
2020-04-24 01:33:39 -0500

    Update mostrarNotificaciones.php

--- conexion/mostrarNotificaciones.php
+++ conexion/mostrarNotificaciones.php
@@ -1,6 +1,6 @@
 <?php
 //session_start();
-require_once("conexionDB.php");
+//require_once("conexionDB.php");
 
 $fechaLocal = date('Y-m-d');
 echo "<h4>Fecha de notificaciones: ".$fechaLocal."</h4>";
@@ -17,19 +17,5 @@ foreach ($rows as $value) {
 
 
 
-/*if($objConexion->buscar($sql) != 0){
-	for($i = 0; $i<$cnt; $i++){
-		echo $row['Notification']."<br>";}}
-else{
-	echo "No se tienen notificaciones para el dia de hoy".$fechaLocal;
-}*/
-//if (isset($_GET['session_name'])) {
-
-	//$_SESSION['session_name'] = $_GET['session_name'];
-	/*while ($reg = mysqli_fetch_array($registro)) {
-		echo $reg['Notification']."<br>"; //muestra la notificación de acuerdo al dia que esta en la variable $fechaDia
-	}*/
-
-//}
 
 ?>
